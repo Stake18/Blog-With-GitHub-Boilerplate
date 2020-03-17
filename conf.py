7f2bb416842f5bed69c7214c11f3e16bd7a869ed
Stake18 <60968460+Stake18@users.noreply.github.com>
2020-03-17 08:12:37 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -19,14 +19,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "测试博客"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+author = "Stake18"
+email = "swjh2000@foxmail.com"
+author_homepage = "https://ignister.cn"
+description = "苟利国家生死以，岂因祸福避趋之。"
+key_words = ['Maverick',  'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
     {
